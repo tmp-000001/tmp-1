515b1a378af1c8c9ddaec9d0b7177920a52b47cf
jjangga0214 <bnbcmindnpass@gmail.com>
2024-02-05 17:22:49 +0900

    tmp

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-import github from '@actions/github'
-import { Octokit } from '@octokit/action'
+import github from "@actions/github";
+import { Octokit } from "@octokit/action";
 
-console.log(JSON.stringify(github.context, null, 2))
-console.log(github.context.payload.sender.login)
+console.log(JSON.stringify(github.context, null, 2));
+console.log(
+  "github.context.payload.sender.login:",
+  github.context.payload.sender.login
+);
 
-const octokit = new Octokit()
-octokit.teams.getMembershipForUserInOrg({
+const octokit = new Octokit();
+// octokit.teams.getMembershipForUserInOrg({
+//   org: context.repo.owner,
+//   team_slug: "tmp",
+//   username: github.context.payload.sender.login,
+// });
+
+const result = await octokit.teams.getMembershipForUserInOrg({
   org: context.repo.owner,
-  team_slug: 'tmp',
-  username: github.context.payload.sender.login
- })
+  team_slug: "tmp",
+  username: github.context.payload.sender.login,
+});
 
-const result = await github.rest.teams.getMembershipForUserInOrg({
-  org: context.repo.owner,
-  team_slug: 'tmp',
-  username: github.context.payload.sender.login
-})
-
-console.log(result)
+console.log("result:", result);
